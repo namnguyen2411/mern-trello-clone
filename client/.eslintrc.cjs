3ab76ead05b87f9f5ca5e9dd0ede0191dc915c3a
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-03-01 00:37:12 +0700

    Install prettier, dnd kit and Material UI packgage

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,42 @@
 module.exports = {
+  settings: {
+    react: { version: 'detect' }
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
       'warn',
-      { allowConstantExport: true },
-    ],
+      {
+        arrowParens: 'always',
+        trailingComma: 'none',
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        printWidth: 120,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        endOfLine: 'auto'
+      }
+    ]
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+  }
 }
