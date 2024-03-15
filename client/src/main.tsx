9f6fc50987ee0a1038adec6ba09c3c82a3a516ee
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-03-15 17:07:38 +0700

    install: axios, react-query package update: create axios instance, http.ts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,24 @@ import App from './App.tsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { theme } from './theme.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
+    <QueryClientProvider client={queryClient}>
+      <CssVarsProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </CssVarsProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
---
+++ client/src/utils/http.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosInstance } from 'axios'
+
+class Http {
+  readonly instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://localhost:8000/api/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+const http = new Http().instance
+export default http
