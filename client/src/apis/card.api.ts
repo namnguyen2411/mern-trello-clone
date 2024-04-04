14e16e75848d054c120a2735cbbef19b2ad4afea
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-04-04 11:09:40 +0700

    feature: update card title and card description

--- client/src/apis/card.api.ts
+++ client/src/apis/card.api.ts
@@ -2,14 +2,19 @@ import { CardType } from 'src/types/card.type'
 import { ColumnType } from 'src/types/column.type'
 import http from 'src/utils/http'
 
+type UpdateCardDataType = Pick<Partial<CardType>, 'title' | 'description' | 'cover'> & { _id: CardType['_id'] }
+
 const createNewCard = async (data: Pick<CardType, 'columnId' | 'boardId' | 'title'>) =>
   (await http.post<CardType>('/cards', data)).data
+
+const updateCard = async (data: UpdateCardDataType) => (await http.put<CardType>(`/cards/${data._id}`, data)).data
 
 const deleteCard = async (id: CardType['_id']) => (await http.delete<Omit<ColumnType, 'cards'>>(`/cards/${id}`)).data
 
 const cardAPI = {
   createNewCard,
-  deleteCard
+  deleteCard,
+  updateCard
 }
 
 export default cardAPI
--- client/src/pages/Board/components/BoardContent/components/Card/Card.tsx
+++ client/src/pages/Board/components/BoardContent/components/Card/Card.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { useConfirm } from 'material-ui-confirm'
-import { Card as MuiCard, CardContent, CardMedia, Typography } from '@mui/material'
-import { Close } from '@mui/icons-material'
+import { EditorProvider } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import {
+  Card as MuiCard,
+  CardContent,
+  CardMedia,
+  Typography,
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField
+} from '@mui/material'
+import { Close, Edit, CheckBox } from '@mui/icons-material'
 import { BoardType } from 'src/types/board.type'
 import { CardType } from 'src/types/card.type'
 import cardAPI from 'src/apis/card.api'
+import EditorMenuBar from './EditorMenuBar'
+
+const extensions = [StarterKit]
 
 type CardProps = {
   card: CardType
@@ -15,6 +32,25 @@ type CardProps = {
 
 export default function Card({ card }: CardProps) {
   const [hoveringCard, setHoveringCard] = useState(false)
+  const [openCardId, setOpenCardId] = useState('')
+  const [openEditCardTitle, setOpenEditCardTitle] = useState(false)
+  const [cardTitle, setCardTitle] = useState(card.title)
+  const [cardDescription, setCardDescription] = useState(card.description)
+
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: card._id,
+    data: { ...card }
+  })
+
+  const dndKitCardStyle = {
+    // Nếu sử dụng CSS.Transform như docs thì gặp lỗi kiểu co giãn
+    // https://github.com/clauderic/dnd-kit/issues/117
+    // Fix => CSS.Translate
+    transform: CSS.Translate.toString(transform),
+    transition,
+    touchAction: 'none',
+    opacity: isDragging ? 0.5 : 1
+  }
 
   const queryClient = useQueryClient()
   const deleteCardMutation = useMutation({
@@ -30,6 +66,32 @@ export default function Card({ card }: CardProps) {
               ...col,
               cards: col.cards.filter((card) => card._id !== deletedCardId),
               cardOrderIds: data.cardOrderIds
+            }
+          })
+        }
+      })
+    }
+  })
+
+  const updateCardMutation = useMutation({
+    mutationFn: cardAPI.updateCard,
+    onSuccess: (data) => {
+      queryClient.setQueryData(['board', card.boardId], (oldData: BoardType) => {
+        if (!oldData) return oldData
+        return {
+          ...oldData,
+          columns: oldData.columns.map((col) => {
+            if (col._id !== data.columnId) return col
+            return {
+              ...col,
+              cards: col.cards.map((card) => {
+                if (card._id !== data._id) return card
+                return {
+                  ...card,
+                  title: data.title,
+                  description: data.description
+                }
+              })
             }
           })
         }
@@ -56,19 +118,36 @@ export default function Card({ card }: CardProps) {
       .catch(() => {})
   }
 
-  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
-    id: card._id,
-    data: { ...card }
-  })
+  const handleOpen = (cardId: string) => {
+    setOpenCardId(cardId)
+    setHoveringCard(false)
+  }
 
-  const dndKitCardStyle = {
-    // Nếu sử dụng CSS.Transform như docs thì gặp lỗi kiểu co giãn
-    // https://github.com/clauderic/dnd-kit/issues/117
-    // Fix => CSS.Translate
-    transform: CSS.Translate.toString(transform),
-    transition,
-    touchAction: 'none',
-    opacity: isDragging ? 0.5 : 1
+  const handleClose = () => {
+    setOpenCardId('')
+    setOpenEditCardTitle(false)
+    setCardTitle(card.title)
+    setCardDescription(card.description)
+  }
+
+  const handleUpdateCardTitle = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>) => {
+    setOpenEditCardTitle(false)
+    setCardTitle(() => {
+      if (e.target.value) return e.target.value
+      return card.title
+    })
+  }
+
+  const handleSubmitUpdateCard = () => {
+    const cardTitleTrimmed = cardTitle.trim()
+    let cardDescriptionTrimmed = (cardDescription as string).trim()
+    if (cardDescriptionTrimmed === '<p></p>') cardDescriptionTrimmed = ''
+    if ((cardTitleTrimmed && cardTitleTrimmed !== card.title) || cardDescriptionTrimmed !== card.description)
+      updateCardMutation.mutate({
+        _id: card._id,
+        title: cardTitleTrimmed,
+        description: cardDescriptionTrimmed
+      })
   }
 
   return (
@@ -106,16 +185,113 @@ export default function Card({ card }: CardProps) {
         />
       )}
 
-      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 }, display: 'flex', justifyContent: 'space-between' }}>
-        <Typography>{card.title}</Typography>
-        {hoveringCard && (
-          <Close
-            sx={{ cursor: 'pointer', '&:hover': { color: 'error.main' } }}
-            fontSize='small'
-            onClick={handleDeleteCard}
-          />
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Box display='flex' justifyContent='space-between' alignItems='center' minHeight='24px'>
+          <Typography>{card.title}</Typography>
+          {hoveringCard && (
+            <Box display='flex' alignItems='center' gap={0.5}>
+              <Edit
+                sx={{ cursor: 'pointer', '&:hover': { color: 'primary.main' } }}
+                fontSize='small'
+                onClick={() => handleOpen(card._id)}
+              />
+              <Close
+                sx={{ cursor: 'pointer', '&:hover': { color: 'error.main' } }}
+                fontSize='medium'
+                onClick={handleDeleteCard}
+              />
+            </Box>
+          )}
+        </Box>
+        {card.description && (
+          <Box display='flex' alignItems='end' gap={0.5} mt={1} fontSize={'14px'}>
+            <CheckBox fontSize='small' /> Description
+          </Box>
         )}
       </CardContent>
+
+      {/* Edit card dialog */}
+      <Dialog open={openCardId !== ''} onClose={handleClose} data-no-dnd='true' maxWidth='xl'>
+        {/* Edit card title */}
+        <DialogTitle
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 1,
+            position: 'relative'
+          }}
+        >
+          Title:
+          {!openEditCardTitle && (
+            <Typography variant='body2' fontSize={'20px'} onClick={() => setOpenEditCardTitle(true)}>
+              {cardTitle}
+            </Typography>
+          )}
+          {openEditCardTitle && (
+            <TextField
+              autoFocus
+              variant='standard'
+              value={cardTitle}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setCardTitle(e.target.value)}
+              onBlur={handleUpdateCardTitle}
+              sx={{
+                '& .MuiInputBase-input': {
+                  fontSize: '20px'
+                }
+              }}
+            />
+          )}
+          <Close
+            fontSize='large'
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              top: '16px',
+              right: '24px',
+              p: 0.5,
+              cursor: 'pointer',
+              borderRadius: '50%',
+              '&:hover': {
+                bgcolor: 'rgba(0, 0, 0, 0.08)'
+              }
+            }}
+          />
+        </DialogTitle>
+
+        {/* Edit card description */}
+        <DialogContent
+          sx={{
+            width: '700px',
+            position: 'relative'
+          }}
+        >
+          <Typography variant='body2' fontSize={'20px'}>
+            Description
+          </Typography>
+          <EditorProvider
+            slotBefore={<EditorMenuBar />}
+            extensions={extensions}
+            content={cardDescription}
+            children={undefined}
+            onUpdate={({ editor }) => {
+              setCardDescription(editor.getHTML())
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            sx={{
+              mr: 1
+            }}
+          >
+            Cancel
+          </Button>
+          <Button type='submit' variant='contained' onClick={handleSubmitUpdateCard}>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </MuiCard>
   )
 }
